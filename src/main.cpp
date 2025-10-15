e3a880f54662e5f29e0c0dfa38bab260f3dc0c30
grigor <musoyangrigor251@gmail.com>
2025-10-15 23:01:05 +0400

    Remove unused includes and obsolete std::srand call

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,7 @@
 #include "TelegramBot.hpp"
-#include <cstdlib>
-#include <ctime>
 #include <string>
 
 int main() {
-    // Seed random number generator for ship placement
-    std::srand(static_cast<unsigned int>(std::time(nullptr)));
-
     // Replace with your actual Telegram Bot API token
     const std::string token = "Your bot token";
 
